27d0f9c0defe639d08653b1f1bfdf8ce0476afa3
Nate-Hunter-max <oppp032@gmail.com>
2023-01-26 21:53:58 +0300

    Merged all files into a library

--- MorseEncoder-master/examples/HelloWorld/HelloWorld.ino
+++ MorseEncoder-master/examples/HelloWorld/HelloWorld.ino
@@ -1,7 +1,6 @@
 //#define MORSE_SERIAL_OUTPUT //define this to send morse code by Serial ('-' is for 1 dot period delay, '>' - 2 dot delay, '==' - 4 dot delay)
 //#define MORSE_PIN_OUTPUT  //define this to send morse code on digital pin (LED_BUILTIN is default)
-//#define MORSE_TONE_OUTPUT 10
-#include "MorseEncoder.h"
+#include <MorseEncoder.h>
 
 char mess[] = "hello world";
 
@@ -15,8 +14,4 @@ void setup() {
 void loop() {
   MorseEncoder.send((char*)mess, sizeof(mess));
   MorseEncoder.update();
-#ifdef MORSE_TONE_OUTPUT
-  if (MorseEncoder.getSignal()) tone(MORSE_TONE_OUTPUT, 15);
-  else noTone(MORSE_TONE_OUTPUT);
-#endif
 }
