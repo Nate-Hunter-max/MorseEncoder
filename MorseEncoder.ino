8a1b0aaf1fd2268890247db1b8edede7d5784229
Nate-Hunter-max <oppp032@gmail.com>
2023-01-26 22:01:02 +0300

    Library hierarchy fix

---
+++ MorseEncoder.ino
@@ -0,0 +1,22 @@
+//#define MORSE_SERIAL_OUTPUT //define this to send morse code by Serial ('-' is for 1 dot period delay, '>' - 2 dot delay, '==' - 4 dot delay)
+//#define MORSE_PIN_OUTPUT  //define this to send morse code on digital pin (LED_BUILTIN is default)
+//#define MORSE_TONE_OUTPUT 10
+#include "src/MorseEncoder.h"
+
+char mess[] = "hello world";
+
+void setup() {
+#ifdef MORSE_SERIAL_OUTPUT
+  Serial.begin(9600);
+#endif
+  MorseEncoder.begin(100);
+}
+
+void loop() {
+  MorseEncoder.send((char*)mess, sizeof(mess));
+  MorseEncoder.update();
+#ifdef MORSE_TONE_OUTPUT
+  if (MorseEncoder.getSignal()) tone(MORSE_TONE_OUTPUT, 15);
+  else noTone(MORSE_TONE_OUTPUT);
+#endif
+}
